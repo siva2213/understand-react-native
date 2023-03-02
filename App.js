d5baa8304dc6b52fc20023c8b91889b902ddd74a
Chettebhaktula Siva Krishna <schettebhaktula@altimetrik.com>
2023-03-02 11:15:52 +0530

    basic app intro

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
-  const 
   return (
-    <View style={styles. styleCon}>
-      <Text>Love you Dhiti</Text>
+    <View style={styles.container}>
+      <View>
+        <Text>Another Text</Text>
+        <Text style={styles.content}>Hello World</Text>
+      </View>
+      <Button title='Tap me'/>
     </View>
   );
 }
+
+// we can use external object for styling but it is not recommended
+// using stylesheet module gives you auto correction of the style properties
+// and as well as errors displayed in browser if any!!
+// const styleObj = {
+//   borderWidth: 2, padding: 6, borderColor: 'red'
+// }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -20,5 +31,6 @@ const styles = StyleSheet.create({
     padding: 2,
     borderWidth: 2,
     borderColor: 'blue'
-  }
+  },
+  content: { borderWidth: 2, padding: 6, borderColor: 'red'}
 });
